fc052037aec8de6c8db0e73163d7137038c1fb3d
yidenek5 <yeabsira.ayele@aastustudent.edu.et>
2026-06-25 09:38:46 +0300

    feat(course): add course model

---
+++ Jakart-EE/src/main/java/com/attendance/model/Course.java
@@ -0,0 +1,49 @@
+package com.attendance.model;
+
+public class Course {
+    private int courseId;
+    private String courseCode;
+    private String courseName;
+
+    public Course() {
+    }
+
+    public Course(int courseId, String courseCode, String courseName) {
+        this.courseId = courseId;
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseId=" + courseId +
+                ", courseCode='" + courseCode + '\'' +
+                ", courseName='" + courseName + '\'' +
+                '}';
+    }
+}
