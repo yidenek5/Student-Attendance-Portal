d8c35f13de918dcfaaee5170f49946402be9046d
yidenek5 <yeabsira.ayele@aastustudent.edu.et>
2026-07-08 23:50:19 +0300

    feat(util): add application constants for session attributes

---
+++ Jakart-EE/src/main/java/com/attendance/util/Constants.java
@@ -0,0 +1,7 @@
+package com.attendance.util;
+
+public final class Constants {
+    public static final String SESSION_TEACHER_ID = "teacherId";
+    public static final String SESSION_TEACHER_NAME = "teacherName";
+    private Constants() {}
+}
